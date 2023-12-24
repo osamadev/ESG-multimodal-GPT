f69745fd89f1ca558743db0a3476f57574a33192
Osama <usama_gis@hotmail.com>
2023-12-24 08:27:24 +0300

    ...

--- pages/🤖_ESG_AI_Strategist_(ESG_GPT).py
+++ pages/🤖_ESG_AI_Strategist_(ESG_GPT).py
@@ -343,8 +343,7 @@ st.set_page_config(page_title="🌱 ESG AI Strategist", page_icon="🌍")
 # Sidebar
 with st.sidebar:
     st.markdown("## ESG Multimodal GPT")
-    st.write("Navigate the path to sustainability with ESG AI Strategist App 🚀💼,where advanced GPT technology meets eco-conscious business strategies  🌱📊.")
-    st.markdown("[TruLens Dashboard](TruLens_Evaluation_Results)", unsafe_allow_html=True) 
+    st.write("Navigate the path to sustainability with ESG AI Strategist App 🚀💼,where advanced GPT technology meets eco-conscious business strategies 🌱📊.")
 
     if st.button("Run TruLens Eval"):
         eval_gemini_completions()
